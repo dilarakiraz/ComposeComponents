4579819e649489de0cdd9092913bec3aff1a1f39
dilarakiraz <dilarakiraz12345@gmail.com>
2024-11-08 00:13:10 +0300

    Chip

---
+++ app/src/main/java/com/example/composecomponents/ui/Chip.kt
@@ -0,0 +1,106 @@
+package com.example.composecomponents.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.AssistChipDefaults
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+private fun Chips() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        item { AssistChip() }
+        item { InputChipExample("Input Chip", onDismiss = {}) }
+        item { FilterChipExample() }
+    }
+}
+
+@Composable
+fun AssistChip(){
+    androidx.compose.material3.AssistChip(
+        onClick = {},
+        label = { Text("Assist Chip") },
+        leadingIcon = {
+            Icon(
+                Icons.Filled.Email,
+                contentDescription = "Localized description",
+                Modifier.size(AssistChipDefaults.IconSize)
+            )
+        }
+    )
+}
+
+@Composable
+fun InputChipExample(
+    text: String,
+    onDismiss: () -> Unit,
+) {
+    var enabled by remember { mutableStateOf(true) }
+    if (!enabled) return
+
+    InputChip(
+        onClick = {
+            onDismiss()
+            enabled = !enabled
+        },
+        label = { Text(text) },
+        selected = enabled,
+        avatar = {
+            Icon(
+                Icons.Filled.Person,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+        trailingIcon = {
+            Icon(
+                Icons.Default.Close,
+                contentDescription = "Localized description",
+                Modifier.size(InputChipDefaults.AvatarSize)
+            )
+        },
+    )
+}
+
+@Composable
+fun FilterChipExample() {
+    val selectedFilter = remember { mutableStateOf(false) }
+
+    FilterChip(
+        selected = selectedFilter.value,
+        onClick = { selectedFilter.value = !selectedFilter.value },
+        label = { Text("Electronics") }
+    )
+}
+
+@Preview
+@Composable
+fun ChipsPreview() {
+    Chips()
+}
